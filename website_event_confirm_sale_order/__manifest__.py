f1f2f2d0d27b8d907c83ced213320113d613f7a1
Ampukajev Arbi <36625431+Sharbe@users.noreply.github.com>
2020-03-17 17:52:41 +0100

    [ADD] Confirm SO when user confirm registration

---
+++ website_event_confirm_sale_order/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Event Confirm Automaticly Registrations",
+    'summary': """Confirm sale orders and attendees for an event
+    """,
+    'author': "ABAKUS IT-SOLUTIONS",
+    'website': "http://www.abakusitsolutions.eu",
+
+    'category': 'Sale',
+    'version': '10.0.1.0',
+
+    'depends': [
+        'event',
+        'sale',
+        'website_sale',
+    ],
+
+    'data': [
+
+    ],
+
+    'installable': True
+}
