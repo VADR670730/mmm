c88693dd9b2c392530314f6b762f4c2b4c506434
Valentin Thirion <valentinthirion@gmail.com>
2020-03-31 11:31:27 +0200

    [MIG] Added report_xls v12.0

--- report_xlsx/__manifest__.py
+++ report_xlsx/__manifest__.py
@@ -1,20 +1,30 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 ACSONE SA/NV (<http://acsone.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     'name': "Base report xlsx",
 
-    'summary': """
-        Base module to create xlsx report""",
+    'summary': "Base module to create xlsx report",
     'author': 'ACSONE SA/NV,'
+              'Creu Blanca,'
               'Odoo Community Association (OCA)',
-    'website': "http://acsone.eu",
+    'website': "https://github.com/oca/reporting-engine",
     'category': 'Reporting',
-    'version': '10.0.1.0.1',
+    'version': '12.0.1.0.2',
     'license': 'AGPL-3',
-    'external_dependencies': {'python': ['xlsxwriter']},
+    'external_dependencies': {
+        'python': [
+            'xlsxwriter',
+            'xlrd',
+        ],
+    },
     'depends': [
-        'base',
+        'base', 'web',
+    ],
+    'data': [
+        'views/webclient_templates.xml',
+    ],
+    'demo': [
+        'demo/report.xml',
     ],
     'installable': True,
 }
